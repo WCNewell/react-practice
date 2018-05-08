b6935df7a9cd6a3e1330a8ce2d886a9dbdcf57cd
Clark Newell <clarknewell@Clarks-MacBook-Pro.local>
2018-05-08 01:50:38 -0600

    first day tutorial

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,28 @@ import { Text, View } from 'react-native'
 
 // Make a component
 const Header = () => {
-    const { textStyle } = styles
-    return <Text style={textStyle}>Albums!</Text>
+    const { textStyle, viewStyle } = styles
+
+    return (
+        <View style={viewStyle}>
+            <Text style = {textStyle}>Albums!</Text>
+        </View>
+    )
 }
 
 const styles = {
+    viewStyle: {
+        backgroundColor: '#27EC27',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        elevation: 2,
+        position: 'relative'
+    },
     textStyle: {
         fontSize: 20
     }
